ede65101e901d8c6f67b8d69d60b53cbeaa59cc3
Jordi Massaguer <jmassaguerpla@suse.de>
2015-04-17 19:09:54 +0200

    add tests for omniauth user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,11 +26,31 @@ RSpec.describe User, type: :model do
     end
   end
   context "2 with the same email" do
-    it "expects an invalide user" do
+    it "expects an invalid user" do
       user1 = User.new(name: "name", email: "a@b.com")
       user1.save
       user2 = User.new(name: "name2", email: "a@b.com")
       expect(user2).to_not be_valid
+    end
+  end
+  context "login with omniauth" do
+    it "expects creating a new user" do
+      auth = { "provider" => "provider", "uid" => "uid", "info" => { "nickname" => "name", "email" => "a@b.com" } }
+      expect {
+        User.from_omniauth(auth)
+      }.to change(User, :count).by(1)
+     
+    end
+    it "expects getting existing user" do
+      auth = { "provider" => "provider", "uid" => "uid2", "info" => { "nickname" => "name", "email" => "b@c.com" } }
+      expect {
+        User.from_omniauth(auth)
+      }.to change(User, :count).by(1)
+     
+      auth = { "provider" => "provider", "uid" => "uid2", "info" => { "nickname" => "name", "email" => "b@c.com" } }
+      expect {
+        User.from_omniauth(auth)
+      }.to_not change(User, :count)
     end
   end
 end
