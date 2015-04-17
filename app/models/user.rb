dd13767ea406d4fe417719764864a1419daf904a
Jordi Massaguer <jmassaguerpla@suse.de>
2015-04-17 18:50:29 +0200

    add authentication with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,20 @@ class User < ActiveRecord::Base
 	   }
     }
   end
+
+  def self.from_omniauth(auth)
+    return nil if auth.nil?
+    return nil unless auth["provider"] && auth["uid"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+  end
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["nickname"]
+      user.email = auth["info"]["email"]
+    end 
+  end
+ 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416122431) do
+ActiveRecord::Schema.define(version: 20150417161413) do
 
   create_table "docker_cfgs", force: :cascade do |t|
     t.string   "url"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20150416122431) do
     t.integer  "docker_cfg_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "uid"
+    t.string   "provider"
   end
 
 end
